bb2faafb780e2f5bbb548a2b0811df178b62c12a
Jianyuan Chen (Jack) <jianyuan.chen12@gmail.com>
2019-03-13 23:43:05 -0700

    Initial Commit

---
+++ ios-segue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-segue
+//
+//  Created by jianyuan chen on 2019-03-13.
+//  Copyright © 2019 Jianyuan Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
